5d507166626abb9574daef6f4e09c8753a5332ea
AntoineZen <antoine.zen@gmail.com>
2016-04-01 22:30:49 +0200

    Done part 1 of driver lab.
    Signed-off-by: AntoineZen <antoine.zen@gmail.com>

---
+++ 03_drivers/01/main.c
@@ -0,0 +1,57 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+       
+// Include for the mmap() function
+#include <sys/mman.h>
+
+int main ()
+{
+	// File descriptor for the /dev/mem file
+	int mem_fd;
+	
+	// Pointer to the ID register (will be set by mmap())
+	uint32_t* id;
+	
+	// Fileds of the ID 	
+	int product_id;
+	int package_id;
+	int major_v;
+	int minor_v;
+	
+	// Open the memory device
+	mem_fd = open("/dev/mem", O_RDONLY);
+	
+	if (mem_fd > 0)
+	{
+		// Map the ID register to our virtual address space
+		id = mmap(NULL, 0x100,  PROT_READ, MAP_PRIVATE, mem_fd, 0x10000000);
+	
+		// Extract the various bit-fields
+    	minor_v 	= (*id & 0x0000000F);
+    	major_v 	= (*id & 0x000000F0) >> 4;
+    	package_id 	= (*id & 0x00000F00) >> 8;
+    	product_id  = (*id & 0xFFFFF000) >> 12;
+    	
+    	// Print the informations readen
+    	printf("Product=%d, package=%d, major=%d, minor=%d\n", product_id, package_id, major_v, minor_v);
+	
+		// Unmap the ID register
+		munmap(id, 0x100);
+		// Close /dev/mem
+		close(mem_fd);
+	}
+	else
+	{
+		printf("Error oppening '/dev/mem'\n");
+	}
+	
+
+	return 0;
+}
--- report/drivers.rst
+++ report/drivers.rst
@@ -5,7 +5,24 @@ Drivers
 Memory Oriented Drivers
 -----------------------
 
-TBD Antoine
+The following  program is able to read direcly some register from the user space using the `/dev/mem` driver:
+
+.. literalinclude:: ../03_drivers/01/main.c
+   :language: c
+   
+   
+If we run the program on the Odroid, we get the following output::
+
+    # cd /usr/workspace/
+    # ls
+    RemoteSystemsTempFiles  mmap_test               sleep.ko
+    iotest.ko               mymodule.ko             xu3
+    # ./mmap_test 
+    Product=939042, package=0, major=0, minor=1
+    # 
+    
+This is the same output we had from the kernel module doing the same job. This means the user space drivers works.
+
 
 
 Charater Oriented Drivers
