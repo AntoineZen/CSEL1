b6ccb88471be733bd26b72fd7cb76aaa7aa62c51
AntoineZen <antoine.zen@gmail.com>
2016-04-18 19:18:37 +0200

    Final optimized version.
    Signed-off-by: AntoineZen <antoine.zen@gmail.com>

--- 06_optimistions/prime.c
+++ 06_optimistions/prime.c
@@ -10,13 +10,15 @@
 #include <string.h>
 
 /*Numbers to test */
-//#define MIN_NUM 1000000
-//#define MAX_NUM 10*MIN_NUM
+#define MIN_NUM 1000000
+#define MAX_NUM 10*MIN_NUM
 
-#define MIN_NUM 1
-#define MAX_NUM 320000
+//#define MIN_NUM 1
+//#define MAX_NUM 320000
 
-void itoa(long value, char* result) { 
+#define BUFFER_SIZE 4096
+
+void itoa(int value, char* result) { 
 
     char* ptr = result, *ptr1 = result, tmp_char; 
     int tmp_value; 
@@ -39,33 +41,69 @@ void itoa(long value, char* result) {
     } 
 } 
 
-int checkNumbers(long long min, long long max, FILE* fd){
-	int  num, div;   
-    char prime;
-    int prime_count = 0;    
+inline int is_prime(int n)
+{
+    register int i = 5;
+    if (n <= 1)
+    {
+        return 0;
+    }
+    else if (n <= 3)
+    {
+        return 1;
+    }
+    else if( n%2 == 0 || n%3 == 0)
+    {
+        return 0;
+    }
+    while (i*i <= n)
+    {
+        if (n%i == 0 || n%(i+2) == 0)
+            return 0;
+        i += 6;
+    }
+    return 1;
+}
 
-    char buffer[32];
 
-	for(num = min; num < max; num++) {
-		prime = 1; 
-    	for(div = 2; div < num/2; div++) {
-	     	if(num%div==0){ 
-			    prime=0; 
-			    break; 
-            }
-    	}
+int checkNumbers(int min, int max, FILE* fd){
+	register int  num;   
+    register int prime_count = 0;    
 
-		if (prime==1) 
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    char* buffer_ptr = buffer;
+
+    // if min is even, skip it
+    if( (min & 0x01) == 0)
+    {
+        min +=1;
+    }
+
+	for(num = min; num < max; num+=2) {
+		if (is_prime(num)) 
         {
             prime_count++;
-            //fprintf(fd, "%d\n", num); 
-            itoa(num, buffer);
-            int len = strlen(buffer);
-            buffer[len] = '\n';
-            buffer[len+1] = '\0';
-            fwrite(buffer, 1,  len+1, fd);
+
+            //printf("%d\n", num); 
+            itoa(num, buffer_ptr);
+            int len = strlen(buffer_ptr);
+            buffer_ptr[len] = '\n';
+            buffer_ptr[len+1] = '\0';
+            buffer_ptr += len+1;
+            //fwrite(buffer,  1, len+1, fd);
+            if (buffer_ptr > buffer + BUFFER_SIZE - 32)
+            {
+                fwrite(buffer, 1, buffer_ptr - buffer, fd);
+                memset(buffer, 0, BUFFER_SIZE);
+                buffer_ptr = buffer;
+            }
         }
+
 	}
+
+    fwrite(buffer, 1, buffer_ptr - buffer, fd);
+
     return prime_count;
 }
 
@@ -82,7 +120,6 @@ prime_task_args;
 
 void* prime_task(void* param)
 {
-
     prime_task_args* args = (prime_task_args*)param;
 
     printf("This is thread %d computing from %i to %i\n", args->n, args->min, args->max);
@@ -95,31 +132,40 @@ int main()
     pthread_t threads[32];
     prime_task_args task_args[32];
 
-
     struct timespec rt1, rt2;
     long long time1;
 
     int n_cpu = sysconf(_SC_NPROCESSORS_ONLN);
 
-
     printf("There is %i cores\n", n_cpu);
 
- 
     FILE* f = fopen("prime_num.txt", "w");
     clock_gettime(CLOCK_REALTIME, &rt1); 
 
-    int slice = (MAX_NUM - MIN_NUM) / n_cpu; 
+    int slice = (MAX_NUM+1 - MIN_NUM) / n_cpu; 
 
+    // Start the threads
     for(int i=0; i < n_cpu; i++)
     {
         task_args[i].n = i;
         task_args[i].min = MIN_NUM + i * slice;
-        task_args[i].max = MIN_NUM + (i+1) * slice -1;
+        if(i == n_cpu-1)
+            task_args[i].max = MAX_NUM;
+        else
+            task_args[i].max = MIN_NUM + (i+1) * slice -1;
         task_args[i].fd = f;
         pthread_create(threads+i, NULL, prime_task, &task_args[i]);
     }
-    checkNumbers(MIN_NUM, MAX_NUM, f); 
+    int prime_count = 0;
+    // Wait for the threads to end
+    for(int i=0; i < n_cpu; i++)
+    {
+        pthread_join(threads[i], NULL);
+        prime_count +=task_args[i].ret;
+    }
     clock_gettime(CLOCK_REALTIME, &rt2);  
+
+    printf("There is %d prime number between %d and %d.\n", prime_count, MIN_NUM, MAX_NUM);
     time1 = (long long int)(rt2.tv_sec - rt1.tv_sec)*1000000000 + (rt2.tv_nsec - rt1.tv_nsec);
     printf("%.3f ms\n", (double)(time1)/1000000);
 
