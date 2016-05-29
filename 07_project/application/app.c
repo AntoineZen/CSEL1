69336552c2dec9ab73c87e5ae6516bbd97824189
AntoineZen <antoine.zen@gmail.com>
2016-05-29 15:50:03 +0200

    Finished the application.
    Signed-off-by: AntoineZen <antoine.zen@gmail.com>

--- 07_project/application/app.c
+++ 07_project/application/app.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
 
 
 static const char help_text[][100] = 
@@ -10,13 +15,15 @@ static const char help_text[][100] =
 	"\n",
 	"Commands are:\n",
 	"\n",
-	" help            : Show this message & exit\n",
-	" mode [auto|man] : Set the control mode\n",
-	" duty [0-100]    : Set the duty-cycle\n",
-	" show            : Show current temperature, duty and mode\n",
-	" exit            : Exit this application\n",
+	" help                  : Show this message & exit\n",
+	" mode [auto|manual]    : Set the control mode\n",
+	" duty [0-100]          : Set the duty-cycle\n",
+	" show                  : Show current temperature, duty and mode\n",
+	" exit                  : Exit this application\n",
 	"\n"
 };
+ 
+static int socket_fd;
 
 void print_help(void)
 {
@@ -30,24 +37,96 @@ void print_help(void)
 
 void set_duty(int duty)
 {
-	printf("Duty setted to %d\n", duty);
+	char buffer[64];
+	// set command text
+	sprintf(buffer, "dutyex=%d\n", duty);
+	// Sent the command
+	write(socket_fd, buffer, strlen(buffer));
+	// Clear buffer for read
+	bzero(buffer, 64);
+	// Read answer
+	read(socket_fd, buffer, 64);
+	// Display answer
+	printf(buffer);
 }
 
 void set_mode(const char* mode)
 {
-	printf("Mode setted to %s\n", mode);
+	char buffer[64];
+	// set command text
+	sprintf(buffer, "mode=%s\n", mode);
+	// Sent the command
+	write(socket_fd, buffer, strlen(buffer));
+	// Clear buffer for read
+	bzero(buffer, 64);
+	// Read answer
+	read(socket_fd, buffer, 64);
+	// Display answer
+	printf(buffer);
 }
 
 void show()
 {
-
+	char buffer[64];
+	// Clear read buffer
+	bzero(buffer, 64);
+	// Sent the command
+	write(socket_fd, "show\n", strlen("show\n"));
+	// Read answer
+	read(socket_fd, buffer, 64);
+	// Displa answer
+	printf(buffer);
 }
+
+int open_socket()
+{
+	// Socket address
+	struct sockaddr_in serv_addr;
+	// Host name
+	struct hostent *server;
+
+	// Create a socket file descriptor
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);	
+	if (sockfd < 0) 
+	{
+		fprintf(stderr,"ERROR opening socket");
+		return -1;
+	}
+
+	// create a host object for host name (here an IP)
+	server = gethostbyname("localhost");
+	if (server == NULL) 
+	{
+		fprintf(stderr,"ERROR, no such host\n");
+		return -1;
+	}
+
+	// Copy server address and port to the socket address structure
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(8080);
+
+
+
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	{
+        fprintf(stderr,"ERROR connecting\n");
+        return -1;
+	}
+
+    return sockfd;
+}
+
+
 
 int main()
 {
 	char input_buffer[100];
 	int duty_work;
 	char mode_work[15];
+
+	socket_fd = open_socket();
 
 	print_help();
 
@@ -74,12 +153,12 @@ int main()
 		else if( strstr(input_buffer, "mode") != NULL)
 		{
 			// Try to parse the mode argument
-			if(sscanf(input_buffer+5, "%s", &mode_work) == 1)
+			if(sscanf(input_buffer+5, "%s", mode_work) == 1)
 			{
 				// Parsing OK, check that the value is legal
-				if (strcmp(mode_work, "auto") != 0  && strcmp(mode_work, "man") != 0)
+				if (strcmp(mode_work, "auto") != 0  && strcmp(mode_work, "manual") != 0)
 				{
-					printf("Error: Mode must be one of \"auto\" or \"man\"!\n");
+					printf("Error: Mode must be one of \"auto\" or \"manual\"!\n");
 					continue;
 				}
 				// All ok, set the mode
@@ -119,8 +198,6 @@ int main()
 			// command was not understood.
 			printf("Error: Unknown command! Type \"help\" for help.\n");
 		}
-
-
 	}
 
 	printf("Bye!\n");
